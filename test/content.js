1f764fe5c9c7fb753814d80037cebb680ff1264c
Jeppe Rune Mortensen <jepperm@gmail.com>
2015-11-28 15:47:57 +0100

    Updated README to include the link to the pre-generated docs.

--- test/content.js
+++ test/content.js
@@ -7,13 +7,19 @@ goog.require("goog.dom.TagName");
 goog.require("goog.events.EventHandler");
 goog.require("goog.events.EventType");
 
+// Prepare injecting the page script into the page.
 var script = goog.dom.createDom(goog.dom.TagName.SCRIPT, {
   'src': chrome.extension.getURL("page.js"),
   'type': 'text/javascript'
 });
+
+// We need to attach a load event to the script element to know when to connect the port.
 var handler = new goog.events.EventHandler();
 handler.listen(script, goog.events.EventType.LOAD, function(){
+  // Connecting the port.
   var port = pl.EventPort.connect("pl-channel", "pl-page-spawner");
+
+  // Check if the port was connected.
   if (port) {
     console.log(port.callMethod("multiply", 2, 3));
 
@@ -25,7 +31,9 @@ handler.listen(script, goog.events.EventType.LOAD, function(){
     console.log("Port was not connected!");
   }
 
+  // Dispose the event handler as it's not needed anymore.
   handler.dispose();
 });
 
+// Injecting the page script into the page.
 (document.body || document.head || document.documentElement).appendChild(script);
--- test/page.js
+++ test/page.js
@@ -2,13 +2,24 @@ goog.provide("pl.test.page");
 
 goog.require("pl.EventPortSpawner");
 
+// Create an instance of the EventPortSpawner.
 pl.test.page = new pl.EventPortSpawner("pl-channel", "pl-page-spawner");
 
+// Listen on ports spawned from the EventPortSpawner.
 pl.test.page.listen(pl.EventPortSpawner.EventType.PORT_SPAWNED, function(e){
+  // Get the port
   var port = e.detail;
-  port.setMethod("multiply", function(a, b){
-    return a * b;
+
+  // Set a port method multiply to multiply the arguments together and return the result.
+  port.setMethod("multiply", function(){
+    var n = arguments[0];
+    for (var i = 1; i < arguments.length; i++) {
+      n = n * arguments[i]
+    }
+    return n;
   });
+
+  // A simple async function with a promise.
   port.setMethod("promiseMe", function(task){
     return new Promise(function(resolve, reject){
       setTimeout(function(){
